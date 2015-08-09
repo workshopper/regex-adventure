bfbecdf59163116bc9ff3a580e0430df01492e0f
James Halliday <mail@substack.net>
2015-08-09 13:07:13 -0700

    literally descriptive messages

--- problems/literally/index.js
+++ problems/literally/index.js
@@ -8,8 +8,9 @@ exports.solution = fs.createReadStream(path.join(__dirname, 'solution.js'))
 exports.verify = verify({ modeReset: true }, function (args, t) {
   t.plan(4)
   var f = require(path.resolve(args[0]))
-  t.ok(f("LITERALLY CAN'T EVEN"))
-  t.notOk(f("FIGURATIVELY can't even"))
-  t.ok(f('I am LITERALLY on a roll down a hill right now'))
-  t.notOk(f('literaturely'))
+  t.ok(f("LITERALLY CAN'T EVEN"), "LITERALLY CAN'T EVEN")
+  t.notOk(f("FIGURATIVELY can't even"), "FIGURATIVELY can't even")
+  t.ok(f('I am LITERALLY on a roll down a hill right now'),
+    'I am LITERALLY on a roll down a hill right now')
+  t.notOk(f('literaturely'), 'literaturely')
 })
